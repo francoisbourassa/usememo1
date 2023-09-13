21cbd5c48e6f26af40b034072e9c86bef72a7758
francois bourassa <you@example.com>
2023-09-13 16:16:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 
+function computeExpensiveValue(num) {
+  console.log("Expensive computation...");
+  // Simulons une opération coûteuse avec une boucle
+  for (let i = 0; i < 1000000000; i++) {}
+  return num * 2;
+}
+
 function App() {
+  const [number, setNumber] = useState(1);
+  const [toggle, setToggle] = useState(true);
+
+  const expensiveValue = useMemo(() => {
+    return computeExpensiveValue(number);
+  }, [number]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setNumber(prevNumber => prevNumber + 1)}>
+          Increment Number
+        </button>
+        <button onClick={() => setToggle(prevToggle => !prevToggle)}>
+          Toggle
+        </button>
+        <div>Number: {number}</div>
+        <div>Expensive Value: {expensiveValue}</div>
+        <div>{toggle ? "Visible" : "Hidden"}</div>
       </header>
     </div>
   );
